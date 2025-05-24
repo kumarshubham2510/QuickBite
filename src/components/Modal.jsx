04e010af9a6e3c59bb7db814b5caa0496949f932
Kumar Shubham <kumarshubhams2510@gmail.com>
2025-05-24 12:15:32 +0530

    cart cart modal created and close function added

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,17 +1,25 @@
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
 import { createPortal } from "react-dom";
+import { useRef } from "react";
+
+import UserProgressContext from "../store/UserProgressContext";
+
 export default function Modal({ children, open, className }) {
-  const dialog = ref();
+  const dialog = useRef();
+
   useEffect(() => {
+    const modal = dialog.current;
     if (open) {
-      dialog.showModal();
+      modal.showModal();
     }
+
+    return () => modal.close();
   }, [open]);
 
   return createPortal(
     <dialog ref={dialog} className={`modal ${className}`}>
       {children}
     </dialog>,
-    document.findElementById("modal")
+    document.getElementById("modal")
   );
 }
